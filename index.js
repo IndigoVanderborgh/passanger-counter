c1c4d923802ead09a48c48153ab3f05b3667363a
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-05 22:52:33 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,22 +2,34 @@ function increment() {
     console.log("The button was clicked")
 }
 
-function countdown() {
-    console.log(5)
-    console.log(4)
-    console.log(3)
-    console.log(2)
-    console.log(1)
-}
 
-// Setting up the the race 🏎 🏎 🏎
+// -----------------------------------------
+// function countdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
 
-countdown()
+// // Setting up the the race 🏎 🏎 🏎
 
-// GO! 🏁
-// Players are running the race 🏎 💨
-// Race is finished! 🍾
+// countdown()
 
-// Get ready for a new race 🏎 🏎 🏎
+// // GO! 🏁
+// // Players are running the race 🏎 💨
+// // Race is finished! 🍾
 
-countdown()
+// // Get ready for a new race 🏎 🏎 🏎
+
+// countdown()
+// -----------------------------------------
+
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+// function myLogger() {
+//     console.log(42)
+// }
+
+// myLogger()

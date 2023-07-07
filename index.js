137dec81f06714fca8ba7cb9b5b063644cd08256
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-06 22:12:20 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+// document.getElementById("count").innerText = 5
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked (log it out)
+// change the count-el in the HTML to reflect the new count
+
+let countEl = document.getElementById("count-el") // pass in arguments
+
+let count = 0
+
 function increment() {
-    console.log("The button was clicked")
+    count = count + 1
+    countEl.innerText = count
 }
+
+// 1. Create a function, save(), which logs out the count when it's called
 
 
 // -----------------------------------------
@@ -12,17 +30,18 @@ function increment() {
 //     console.log(1)
 // }
 
-// // Setting up the the race 🏎 🏎 🏎
+// // Setting up the the race 
 
 // countdown()
 
 // // GO! 🏁
-// // Players are running the race 🏎 💨
+// // Players are running the race 💨
 // // Race is finished! 🍾
 
-// // Get ready for a new race 🏎 🏎 🏎
+// // Get ready for a new race 
 
 // countdown()
+
 // -----------------------------------------
 
 // Create a function (you decide the name) that logs out the number 42 to the console
@@ -33,3 +52,35 @@ function increment() {
 // }
 
 // myLogger()
+
+// -----------------------------------------
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// // Create a function that logs out the sum of all the lap times
+// function logLapTime() {
+//     let totalTime = lap1 + lap2 + lap3
+//     console.log(totalTime)
+// }
+
+// // console.log(totalTime)
+
+// // console.log(totalTime) will not work, because the variable totalTime is instanced in the function only and cannot be called outside. Therefor it is better to use logLapTime()
+
+// -----------------------------------------
+
+// let lapsCompleted = 0
+// function lapsIncrement() {
+//     lapsCompleted = lapsCompleted + 1
+// }
+// lapsIncrement()
+// lapsIncrement()
+// lapsIncrement()
+
+// console.log(lapsCompleted)
+
+// -----------------------------------------
+
+
